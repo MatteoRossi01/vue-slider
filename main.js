294869181a9fc6d558575991303b3d2f4a035440
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-02-10 19:09:25 +0100

    Esercizio completato

--- main.js
+++ main.js
@@ -5,28 +5,28 @@ const app = new Vue (
 
             items : [
                 {
-                    name:'Svezia',
                     url:'img/01.jpg',
+                    name:'Svezia',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
                 },
                 {
-                    name:'Svizzera',
                     url:'img/02.jpg',
+                    name:'Svizzera',
                     text:'Lorem ipsum'
                 },
                 {
-                    name:'Gran Bretagna',
                     url:'img/03.jpg',
+                    name:'Gran Bretagna',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
                 },
                 {
-                    name:'Germania',
                     url:'img/04.jpg',
+                    name:'Germania',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
                 },
                 {
-                    name:'Paradise',
                     url:'img/05.jpg',
+                    name:'Paradise',
                     text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
                 }
             ],
@@ -35,108 +35,27 @@ const app = new Vue (
 
         methods: {
 
+            /* Funzione click per selezionare l'immagine precedente */
+            imgPrec() {
+
+                this.active--;
+
+                if(this.active < 0){
+                    this.active = this.items.length -1;
+                }
+            },
+
+            /* Funzione click per selezionare l'immagine successiva */
+            imgSucc() {
+
+                this.active++;
+
+                if(this.active == this.items.length){
+                    this.active = 0;
+                }
+            },
             
         }
 
     }
 );
-
-
-
-/* const items = [
-    {
-        photo: 'img/01.jpg',
-        title: "Svezia",
-        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
-    },
-    {
-        photo: 'img/02.jpg',
-        title: "Svizzera",
-        text: "Lorem ipsum."
-    },
-    {
-        photo: 'img/03.jpg',
-        title: "Gran Bretagna",
-        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-    },
-    {
-        photo: 'img/04.jpg',
-        title: "Germania",
-        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
-    },
-    {
-        photo: 'img/05.jpg',
-        title: "Paradise",
-        text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
-    }
-];
-
-const itemsRef = document.getElementsByClassName('items')[0];
-const thumbsRef = document.getElementsByClassName('thumbs')[0];
-let item = '';
-let thumb = '';
-let active = 1;
-
-
-/* Versione ciclo for con gli oggetti 
-for (let i=0; i<items.length; i++){
-
-    item += `
-        <div class="item">
-            <img src="${items[i].photo}" alt="">
-            <div class="text">
-                <h3>${items[i].title}</h3>
-                <p>${items[i].text}</p>
-            </div>
-        </div>`
-    thumb += `
-        <div class="thumb">
-            <img src="${items[i].photo}" alt="">
-        </div>` 
-}
-
-itemsRef.innerHTML = item;
-document.getElementsByClassName('item')[active].classList.add('active');
-
-thumbsRef.innerHTML += thumb;
-document.getElementsByClassName('thumb')[active].classList.add('active');
-
-const prev = document.querySelector('.prev');
-prev.addEventListener('click', function() {
-    if(active == 0) {                                                                   
-        active = items.length - 1; 
-
-        document.querySelector('.item.active').classList.remove('active');
-        document.getElementsByClassName('item')[active].classList.add('active');
-
-        document.querySelector('.thumb.active').classList.remove('active');
-        document.getElementsByClassName('thumb')[active].classList.add('active');
-    } else if(active < items.length) {
-        --active
-        document.querySelector('.item.active').classList.remove('active');
-        document.getElementsByClassName('item')[active].classList.add('active');
-
-        document.querySelector('.thumb.active').classList.remove('active');
-        document.getElementsByClassName('thumb')[active].classList.add('active');
-    } 
-});
-
-const next = document.querySelector('.next');
-next.addEventListener('click', function() {
-    if(active < items.length - 1) {
-        ++active
-        document.querySelector('.item.active').classList.remove('active');
-        document.getElementsByClassName('item')[active].classList.add('active');
-
-        document.querySelector('.thumb.active').classList.remove('active');
-        document.getElementsByClassName('thumb')[active].classList.add('active');
-    } else if(active == items.length - 1) { 
-        active = 0;
-        document.querySelector('.item.active').classList.remove('active');
-        document.getElementsByClassName('item')[active].classList.add('active');
-
-        document.querySelector('.thumb.active').classList.remove('active');
-        document.getElementsByClassName('thumb')[active].classList.add('active');
-    }
-}); */
-
